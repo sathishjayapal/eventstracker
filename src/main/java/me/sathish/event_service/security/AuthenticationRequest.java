37d9d44989ec9f61a11d823b67941b220a16d031
Bootify <>
2025-06-01 15:00:25 +0000

    add security config eventserviceconfig

---
+++ src/main/java/me/sathish/event_service/security/AuthenticationRequest.java
@@ -0,0 +1,21 @@
+package me.sathish.event_service.security;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class AuthenticationRequest {
+
+    @NotNull
+    @Size(max = 255)
+    private String login;
+
+    @NotNull
+    @Size(max = 72)
+    private String password;
+
+}
