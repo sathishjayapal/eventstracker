36041e0434f4968b200de1f7658061cd5202d697
Sathish Jayapal <sjayapal@skminfotech.com>
2025-09-22 21:29:20 -0500

    Queue error messaging

--- src/main/java/me/sathish/event_service/util/ApplicationProperties.java
+++ src/main/java/me/sathish/event_service/util/ApplicationProperties.java
@@ -3,4 +3,4 @@ package me.sathish.event_service.util;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "sathishprojects")
-public record ApplicationProperties(String gitHubExchange, String githubRoutingKey) {}
+public record ApplicationProperties(String sathishProjectEventsExchange, String githubRoutingKey) {}
